9222ae41339371073a5e6b0e44bcfc5c28f211e4
Nanako Yagi <nanako2752@gmail.com>
2026-06-14 09:50:28 +0900

    削除対応

--- public/js/gallery-menu.js
+++
@@ -1,8 +0,0 @@
-
-const galleryForm = document.getElementById("js-gallery-select-box")
-
-galleryForm.addEventListener("change", (e) => {
-    console.log(e.currentTarget.value)
-
-    location.href = e.currentTarget.value
-})
